bff5fde0f2a305799d931ed2a28bfd10d025e898
ymndlz <ymn.casual@gmail.com>
2021-04-17 13:25:48 -0400

    instantnoodlep: Update fingerprint to Pixel 5. (RQ2A.210405.005)
    Change-Id: I8d229cc05994dc1ca5b0d2cb8225a67e4a2ba103

--- lineage_instantnoodlep.mk
+++ lineage_instantnoodlep.mk
@@ -35,6 +35,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8Pro \
     PRODUCT_NAME=OnePlus8Pro_EEA \
-    PRIVATE_BUILD_DESC="OnePlus8Pro-user 11   release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:10/QKQ1.191222.002/2005301332:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
